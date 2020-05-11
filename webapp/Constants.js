c4b349f05119641484d994fdbd4feb53c09d678d
MorarStefan <stefan.morar30@gmail.com>
2020-05-11 14:27:19 +0000

    Story 2: #24 FE: Master page: Integration with back-end to display users.

---
+++ webapp/Constants.js
@@ -0,0 +1,12 @@
+sap.ui.define([],
+function() {
+    "use strict";
+
+    var constants = {
+        BASE_URL: "http://localhost:8083",
+        USERS_PATH: "/users",
+        BOOKINGS_PATH: "/bookings"
+    };
+
+    return constants;
+});
--- webapp/controller/Master.controller.js
+++ webapp/controller/Master.controller.js
@@ -1,18 +1,33 @@
 sap.ui.define([
+	"../Constants",
 	"sap/ui/model/json/JSONModel",
 	"sap/ui/core/mvc/Controller",
 	"sap/ui/model/Filter",
 	"sap/ui/model/FilterOperator",
 	'sap/ui/model/Sorter',
 	'sap/m/MessageBox'
-], function (JSONModel, Controller, Filter, FilterOperator, Sorter, MessageBox) {
+], function (Constants, JSONModel, Controller, Filter, FilterOperator, Sorter, MessageBox) {
 	"use strict";
 
 	return Controller.extend("sap.ui.demo.fiori2.controller.Master", {
 		onInit: function () {
 			this.oView = this.getView();
+
+			var oModel = new sap.ui.model.json.JSONModel();
+
+			$.ajax({
+				type: "GET",
+				contentType: "application/json",
+				url: Constants.BASE_URL + Constants.USERS_PATH,                 
+				dataType: "json",
+				async: false, 
+				success: function (data, textStatus, jqXHR) {
+					oModel.setProperty("/modelData", data);
+				}
+			});
+
+			this.oView.setModel(oModel, "users");
 		}
 
 	});
 });
- 
