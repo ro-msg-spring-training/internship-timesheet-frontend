90095ff9b93d84e866a992ebc626ac449028227d
VadeanAndrada <andrada.vadean@gmail.com>
2020-05-14 12:40:26 +0000

    Story 4: #32 FE: Create delete button and backend integration for it: upgrade delete for multiple rows

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -1,8 +1,10 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"../Constants",
-	"sap/f/library"
-], function (Controller, Constants, fioriLibrary) {
+	"sap/f/library",
+	"sap/m/MessageToast",
+	"sap/ui/core/routing/History"
+], function (Controller, Constants, fioriLibrary, MessageToast, History) {
 	"use strict";
 
 	return Controller.extend("sap.ui.demo.fiori2.controller.Detail", {
@@ -53,7 +55,7 @@ sap.ui.define([
 			var booking = this.oView.getModel("bookings").getData().bookingData;
 			var bookingId = booking[bookingPos].bookingId;
 			console.log(bookingId);
-			 this.oRouter.navTo("createBookingDetail", {
+			this.oRouter.navTo("createBookingDetail", {
 				layout: fioriLibrary.LayoutType.TwoColumnsMidExpanded,
 				user: this._user,
 				programName: this._programName,
@@ -67,50 +69,59 @@ sap.ui.define([
 				url: Constants.BASE_URL + Constants.BOOKING_DETAILS_PATH + "/" + idBookingDetail,
 				dataType: "json",
 				async: false,
-				success: function (data, textStatus, jqXHR) {
-				}
-			}).done(function (data) {
-				console.log(data)
-			});
+				success: function (data, textStatus, jqXHR) {}
+			}).done(function (data) {});
 		},
 		onDelete: function (oEvent) {
 			var oTable = this.byId("bookingsTable");
-			var seletctedItem = oTable.getSelectedIndex();
-			var bookingDetailPos = oTable.getContextByIndex(seletctedItem).getPath().split("/")[4];
-			var bookingPos = oTable.getContextByIndex(seletctedItem).getPath().split("/")[2];
-			var booking = this.oView.getModel("bookings").getData().bookingData;
-			var bookingDetailId = booking[bookingPos].bookingDetails[bookingDetailPos].id;
-			this._deleteBookingDetail(bookingDetailId);
-			this.onInit();
+			var aSelectedIndices = oTable.getSelectedIndices();
+			if (aSelectedIndices.length === 0) {
+				MessageToast.show("Select at least one booking detail first!");
+				return;
+			}
+			for (var j = 0; j < aSelectedIndices.length; j++) {
+				var bookingDetailPosition = oTable.getContextByIndex(aSelectedIndices[j]).getPath().split("/")[4];
+				if (bookingDetailPosition === undefined) {
+					MessageToast.show("Select just booking details, please!");
+					return;
+				}
+			}
+			for (var i = 0; i < aSelectedIndices.length; i++) {
+				var bookingDetailPos = oTable.getContextByIndex(aSelectedIndices[i]).getPath().split("/")[4];
+				var bookingPos = oTable.getContextByIndex(aSelectedIndices[i]).getPath().split("/")[2];
+				var booking = this.oView.getModel("bookings").getData().bookingData;
+				var bookingDetailId = booking[bookingPos].bookingDetails[bookingDetailPos].id;
+				this._deleteBookingDetail(bookingDetailId);
+				history.go();
+			}
 		},
 		onMakeEditable: function (oEvent) {
 			var oTable = this.getView().byId("bookingsTable");
 			var selectedItem = oTable.getSelectedIndex();
-			
+
 			var iBookingDetailPos;
 			var iPos = 0;
-			for(var i = 0; i <= parseInt(selectedItem); i ++) {
+			for (var i = 0; i <= parseInt(selectedItem); i++) {
 				iBookingDetailPos = oTable.getContextByIndex(selectedItem).getPath().split("/")[4];
 				iPos++;
 			}
-			
+
 			console.log(iPos);
-			
+
 			var aItems = oTable.getRows();
-			
+
 			console.log(aItems);
 
-			for(var i = 0; i < aItems.length; i ++) {
-				if(i == selectedItem) {
+			for (var i = 0; i < aItems.length; i++) {
+				if (i == selectedItem) {
 					aItems[i].getCells()[0].setEnabled(true);
 					aItems[i].getCells()[1].setEnabled(true);
 					aItems[i].getCells()[2].setEnabled(true);
 					aItems[i].getCells()[4].setEnabled(true);
 					aItems[i].getCells()[5].setEnabled(true);
-					
+
 					console.log(aItems[i].getCells());
-				}
-				else {
+				} else {
 					aItems[i].getCells()[0].setEnabled(false);
 					aItems[i].getCells()[1].setEnabled(false);
 					aItems[i].getCells()[2].setEnabled(false);
@@ -118,9 +129,8 @@ sap.ui.define([
 					aItems[i].getCells()[5].setEnabled(false);
 				}
 			}
-			
-		}
 
+		}
 
 	});
 });
